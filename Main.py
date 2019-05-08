6f0677ad130618517d8a9db136c4bcb949458396
DBockrath <dbockrathdev@gmail.com>
2019-05-08 14:03:20 -0400

    Added edge detection with opencv.

--- Main.py
+++ Main.py
@@ -2,9 +2,15 @@ import numpy as np
 from PIL import ImageGrab
 import cv2
 
+def process_img(original_image):
+    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+    return processed_img
+
 while (True):
     screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
-    cv2.imshow('window', screen)
+    new_screen = process_img(screen)
+    cv2.imshow('Screen', cv2.cvtColor(new_screen, cv2.COLOR_BGR2RGB))
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
