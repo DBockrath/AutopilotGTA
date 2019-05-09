03a73a7e73244eec291d37a59570be51001b4318
DBockrath <dbockrathdev@gmail.com>
2019-05-09 13:49:31 -0400

    Added a region of interest filter.

--- main.py
+++ main.py
@@ -3,15 +3,24 @@ from PIL import ImageGrab
 import cv2
 import time
 from directkeys import PressKey, ReleaseKey, W, A, S, D
+import pyautogui
+
+def roi(img, vertices):
+    mask = np.zeros_like(img)
+    cv2.fillPoly(mask, vertices, 255)
+    masked = cv2.bitwise_and(img, mask)
+    return masked
 
 def process_img(original_image):
     processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
     processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+    vertices = np.array([[10, 500], [10, 300], [300, 200], [500, 200], [800, 300], [800, 500]])
+    processed_img = roi(processed_img, [vertices])
     return processed_img
 
-for i in list(range(4))[::-1]:
-    print(i+1)
-    time.sleep(1)
+# for i in list(range(4))[::-1]:
+#     print(i+1)
+#     time.sleep(1)
 
 while (True):
     screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
